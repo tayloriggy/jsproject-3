31826ca87f0a02266f1785e0a77e2c285df129f2
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-10-09 23:25:27 -0500

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+Project 3: Build a number-guessing game.
+
+Requirements:
+
+-Create a Github repository for this project. 
+-A user has three tries to guess the number, between 1-100, that the computer picked.
+-Whenever they guess, let them know if their guess is too high, or too low.
+-After their third guess, reveal the number the computer chose to them, and let them know if they got it right or wrong.
+-Use an HTML form to create an input input field for the user to enter their guess
+-Add a "Submit" button to the form, and use the "onsubmit" attribute to call your JavaScript function.
+*/
+
+function guessNumber () {
+    
+}

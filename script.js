16ef3fa8ddd16635e8bc5251fd6467a1eae009a8
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-10-13 16:00:06 -0500

    update

--- script.js
+++ script.js
@@ -9,8 +9,36 @@ Requirements:
 -After their third guess, reveal the number the computer chose to them, and let them know if they got it right or wrong.
 -Use an HTML form to create an input input field for the user to enter their guess
 -Add a "Submit" button to the form, and use the "onsubmit" attribute to call your JavaScript function.
+
+If the input number matches the computer picked number, use alert to say that they got it right.
+Else use alert that it is wrong.
+Use while loop to set numberOfGuesses to 3? Once it gets to a third guess, print out the computer's number and alert if
+the picked number is right or wrong.
 */
 
+const randomNum = Math.floor(Math.random() * 100 + 1); //generates a computer randomized number between 1-100
+let numberOfGuesses = 1; //counts the number of guesses made for correct guess 
+
 function guessNumber () {
+    let numberGuessed = document.getElementById("guessNumber").value;
+
+    if (numberGuessed === randomNum) {
+            alert ("You got it right!");
+    }
+    else if (numberGuessed < randomNum){
+        numberOfGuesses++;
+        alert ("Too low");
+    }
+    else if (numberGuessed > randomNum){
+        numberOfGuesses++;
+        alert ("Too high");
+    }
+    else {
+        alert ("Wrong");
+    }
+
+    return `The correct number is ${randomNum}`;
+
+
     
 }
